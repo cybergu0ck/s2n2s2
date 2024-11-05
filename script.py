e08614dcb577b7636f394b27fd4192a6928a0d15
agneshraok <agneshraok.apps@gmail.com>
2024-11-05 21:36:40 +0530

    Saving progress - Completed the TODOs - Created modularised functions - Created encoding for headers - Added docstrings for global functions

--- script.py
+++ script.py
@@ -14,39 +14,95 @@ from twilio.rest import Client
 
 
 # Global Variables
-today_date = datetime.today().strftime("%d/%m/%Y")
+TODAY = datetime.today().strftime("%d/%m/%Y")
+HEADER_ROW = 1
+
+RECEIPT_NUMBER = 1
+RECEIPT_DATE = 2
+REGISTERED_TITLE = 3
+REGISTERED_NAME = 4
+REGISTERED_DATE = 5
+REGISTERED_GOTRA = 6
+REGISTERED_NAKSHATRA = 7
+REGISTERED_RASHI = 8
+REGISTERED_PHONE_NUMBER = 9
+REGISTERED_WHATSAPP_NUMBER = 10
+
+HEADER_ENCODING = {
+    "Receipt Number": RECEIPT_NUMBER,
+    "Receipt Date": RECEIPT_DATE,
+    "Name": REGISTERED_NAME,
+    "Date": REGISTERED_DATE,
+    "Gotra": REGISTERED_GOTRA,
+    "Nakshatra": REGISTERED_NAKSHATRA,
+    "Rashi": REGISTERED_RASHI,
+    "Phone Number": REGISTERED_PHONE_NUMBER,
+    "Whatsapp Number": REGISTERED_WHATSAPP_NUMBER,
+}
+
+
 # NOTE - The creds are stored as env variables
 TWILIO_ACC_SSID = os.getenv("TWILIO_ACC_SSID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 
 
 # Global Functions
-def is_date(value):
+def is_valid_date(value):
     try:
-        datetime.strptime(value, "%m/%d/%Y")
+        datetime.strptime(value, "%d/%m/%Y")
         return True
     except (ValueError, TypeError):
         return False
 
 
-def create_val_to_address_map(worksheet):
-    headings = worksheet.row_values(1)  # TODO - This function can be written better
-    row_map = {}
-    for idx, value in enumerate(headings):
-        # cell_address = f"{chr(65 + idx)}1"
-        cell_address = idx + 1
-        row_map[value] = cell_address
-    return row_map
+def get_encoded_header_id_map(worksheet):
+    """
+    Returns a map with keys as encoded headers (encoded column headings) and the values as columen id.
+    - This ensures that script will not fail if columns are interchanged or modified.
+    - The
+    - Example: {nakshatra: 5, rashi : 6}
+    """
+    res = {}
+    header_row_values = worksheet.row_values(HEADER_ROW)
+    for id, header in enumerate(header_row_values):
+        col_id = id + 1
+        res[HEADER_ENCODING[header]] = col_id
+    return res
 
 
-def get_rows_with_today_date(worksheet):
-    matching_rows = []
-    date_column_values = worksheet.col_values(heading_map["Date"])
-    for row_idx, cell_value in enumerate(date_column_values[1:], start=2):
-        if cell_value == today_date:
-            row_values = worksheet.row_values(row_idx)
-            matching_rows.append(row_values)
-    return matching_rows
+def get_todays_recepients(worksheet, encoded_header_to_id):
+    """
+    Returns a list containing list of all the values of a row whose date corresponds to today's date.
+    """
+    res = []
+    date_column_values = worksheet.col_values(encoded_header_to_id["date"])
+    for row_id, cell_value in enumerate(date_column_values[1:], start=2):
+        if is_valid_date(cell_value):
+            if cell_value == TODAY:
+                row_values = worksheet.row_values(row_id)
+                res.append(row_values)
+    return res
+
+
+def get_custom_message(**kwargs):
+    """
+    Returns the custom text for the input parameters
+    """
+    res = f"Namasthe {kwargs.get('title')} {kwargs.get('name')}, Greetings from Naalur. As per Receipt Number {kwargs.get('receipt_num')}, dated {kwargs.get('receipt_date')}, Pooje has been done in the name of {kwargs.get('name')} of {kwargs.get('gotra')} Gotra, {kwargs.get('nakshatra')} Nakshtra, {kwargs.get('rashi')} Rashi. May the blessings of Naalur Devasthana always be with you."
+
+
+def send_whatsapp_text(text, number):
+    """
+    Sends the text over whatsapp to the provided number
+    """
+    account_sid = TWILIO_ACC_SSID
+    auth_token = TWILIO_AUTH_TOKEN
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=text,
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+" + number,
+    )
 
 
 # Business Logic
@@ -54,27 +110,22 @@ def main():
     gc = gspread.service_account()
     sheet = gc.open("s2n2s2-db")
     worksheet = sheet.sheet1
-    global heading_map
-    heading_map = create_val_to_address_map(worksheet)
-    todays_devotees = get_rows_with_today_date(worksheet)
-    for devotee in todays_devotees:
-        name = devotee[1]  # TODO - Work a way to get rid of the hardcoding
-        receipt_date = devotee[2]
-        receipt_num = devotee[3]
-        gotra = devotee[5]
-        nakshatra = devotee[6]
-        rashi = devotee[7]
-        phone_num = devotee[8]
-        whatsapp_num = devotee[9]
-        custom_message = f"Hello Mr.{name}, Greetings from Naalur. As per Receipt Number {receipt_num}, dated {receipt_date}, Pooje has been done in the name of {name} of {gotra} Gotra, {nakshatra} Nakshtra, {rashi} Rashi. May the blessings of Naalur Devasthana always be with you."
-        account_sid = TWILIO_ACC_SSID
-        auth_token = TWILIO_AUTH_TOKEN
-        client = Client(account_sid, auth_token)
-        message = client.messages.create(
-            body=custom_message,
-            from_="whatsapp:+14155238886",
-            to="whatsapp:+" + whatsapp_num,
+    encoded_header_to_id = get_encoded_header_id_map(worksheet)
+    recipients = get_todays_recepients(worksheet, encoded_header_to_id)
+    for recipient in recipients:
+        phone_num = recipient[encoded_header_to_id[REGISTERED_PHONE_NUMBER]]
+        whatsapp_num = recipient[encoded_header_to_id[REGISTERED_WHATSAPP_NUMBER]]
+        custom_message = get_custom_message(
+            title=recipient[encoded_header_to_id[REGISTERED_TITLE]],
+            name=recipient[encoded_header_to_id[REGISTERED_NAME]],
+            receipt_num=recipient[encoded_header_to_id[RECEIPT_NUMBER]],
+            receipt_date=recipient[encoded_header_to_id[REGISTERED_DATE]],
+            gotra=recipient[encoded_header_to_id[REGISTERED_GOTRA]],
+            nakshatra=recipient[encoded_header_to_id[REGISTERED_NAKSHATRA]],
+            rashi=recipient[encoded_header_to_id[REGISTERED_RASHI]],
         )
+        send_whatsapp_text(custom_message, whatsapp_num)
+        # TODO - Similarly send sms text
 
 
 if __name__ == "__main__":
