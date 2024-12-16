dc8e5e24e288dd20f7bfac0e0c8e709d66c479c1
Agnesh Rao K <89940933+cybergu0ck@users.noreply.github.com>
2024-12-16 23:57:45 +0530

    Fix problems with maintaining internal reference of column headers

--- script.py
+++ script.py
@@ -3,47 +3,51 @@
   * This script access the google sheet, parses it and uses the relevant information to send messages via a client.
   * This script is supposed to be deployed in a server where it will be executed once, everyday.
 """
+"""
+DEV NOTES:
+
+Keep the code simple and donot introduce unnecesary classes.
+Add exception handling and in the case of exception send fail log to admin number
+"""
+
 
 import gspread
-from datetime import datetime
 import os
+from datetime import datetime
+from enum import Enum, auto
 
-
-# DEVNOTE - Choosing not to create a class (atleast in this stage) and proceeding with global variables and global utility functions to keep it simple and functional.
-
-
-# Global Variables
-GOOGLE_SHEETS_TITLE = "s2n2s2-db"
-
+SHEETS_TITLE = "s2n2s2-db"
 TODAY = datetime.today().strftime("%d/%m/%Y")
 HEADER_ROW = 1
 
-RECEIPT_NUMBER = 1
-RECEIPT_DATE = 2
-REGISTERED_TITLE = 3
-REGISTERED_NAME = 4
-REGISTERED_DATE = 5
-REGISTERED_GOTRA = 6
-REGISTERED_NAKSHATRA = 7
-REGISTERED_RASHI = 8
-REGISTERED_PHONE_NUMBER = 9
-REGISTERED_WHATSAPP_NUMBER = 10
 
-HEADER_ENCODING = {
-    "Receipt Number": RECEIPT_NUMBER,
-    "Receipt Date": RECEIPT_DATE,
-    "Title": REGISTERED_TITLE,
-    "Name": REGISTERED_NAME,
-    "Date": REGISTERED_DATE,
-    "Gotra": REGISTERED_GOTRA,
-    "Nakshatra": REGISTERED_NAKSHATRA,
-    "Rashi": REGISTERED_RASHI,
-    "Phone Number": REGISTERED_PHONE_NUMBER,
-    "Whatsapp Number": REGISTERED_WHATSAPP_NUMBER,
+class SheetsHeader(Enum):
+    RECEIPT_NUMBER = auto()
+    RECEIPT_DATE = auto()
+    REGISTERED_TITLE = auto()
+    REGISTERED_NAME = auto()
+    REGISTERED_DATE = auto()
+    REGISTERED_GOTRA = auto()
+    REGISTERED_NAKSHATRA = auto()
+    REGISTERED_RASHI = auto()
+    REGISTERED_PHONE_NUMBER = auto()
+    REGISTERED_WHATSAPP_NUMBER = auto()
+
+
+sheetsheader_to_internalreference = {
+    "Receipt Number": SheetsHeader.RECEIPT_NUMBER,
+    "Receipt Date": SheetsHeader.RECEIPT_DATE,
+    "Title": SheetsHeader.REGISTERED_TITLE,
+    "Name": SheetsHeader.REGISTERED_NAME,
+    "Date": SheetsHeader.REGISTERED_DATE,
+    "Gotra": SheetsHeader.REGISTERED_GOTRA,
+    "Nakshatra": SheetsHeader.REGISTERED_NAKSHATRA,
+    "Rashi": SheetsHeader.REGISTERED_RASHI,
+    "Phone Number": SheetsHeader.REGISTERED_PHONE_NUMBER,
+    "Whatsapp Number": SheetsHeader.REGISTERED_WHATSAPP_NUMBER,
 }
 
 
-# Global Functions
 def is_valid_date(value):
     try:
         datetime.strptime(value, "%d/%m/%Y")
@@ -52,9 +56,9 @@ def is_valid_date(value):
         return False
 
 
-def get_encoded_header_id_map(worksheet):
+def get_internalheader_to_columnid(worksheet):
     """
-    Returns a map with keys as encoded headers (encoded column headings) and the values as columen id.
+    Returns a map with keys as internal header references (encoded column headings) and the values as columen id.
     - This ensures that script will not fail if columns are interchanged or modified.
     - The
     - Example: {nakshatra: 5, rashi : 6}
@@ -63,16 +67,18 @@ def get_encoded_header_id_map(worksheet):
     header_row_values = worksheet.row_values(HEADER_ROW)
     for id, header in enumerate(header_row_values):
         col_id = id + 1
-        res[HEADER_ENCODING[header]] = col_id
+        res[sheetsheader_to_internalreference[header]] = col_id
     return res
 
 
-def get_todays_recepients(worksheet, encoded_header_to_id):
+def get_todays_recepients(worksheet, internalheader_to_columnid):
     """
     Returns a list containing list of all the values of a row whose date corresponds to today's date.
     """
     res = []
-    date_column_values = worksheet.col_values(encoded_header_to_id["date"])
+    date_column_values = worksheet.col_values(
+        internalheader_to_columnid[SheetsHeader.REGISTERED_DATE]
+    )
     for row_id, cell_value in enumerate(date_column_values[1:], start=2):
         if is_valid_date(cell_value):
             if cell_value == TODAY:
@@ -92,28 +98,37 @@ def send_whatsapp_text(text, number):
     """
     Sends the text over whatsapp to the provided number
     """
-    #STUB - Work on this once the communication over sms is made stable
+    # STUB - Work on this once the communication over sms is made stable
 
 
-# Business Logic
 def main():
-    # TODO - Add exception handling and in the case of exception send fail log to admin number
     gc = gspread.service_account()
-    sheet = gc.open(GOOGLE_SHEETS_TITLE)
+    sheet = gc.open(SHEETS_TITLE)
     worksheet = sheet.sheet1
-    encoded_header_to_id = get_encoded_header_id_map(worksheet)
-    recipients = get_todays_recepients(worksheet, encoded_header_to_id)
+
+    internalheader_to_columnid = get_internalheader_to_columnid(worksheet)
+    recipients = get_todays_recepients(worksheet, internalheader_to_columnid)
     for recipient in recipients:
-        phone_num = recipient[encoded_header_to_id[REGISTERED_PHONE_NUMBER]]
-        whatsapp_num = recipient[encoded_header_to_id[REGISTERED_WHATSAPP_NUMBER]]
+        phone_num = recipient[
+            internalheader_to_columnid[SheetsHeader.REGISTERED_PHONE_NUMBER]
+        ]
+        whatsapp_num = recipient[
+            internalheader_to_columnid[SheetsHeader.REGISTERED_WHATSAPP_NUMBER]
+        ]
         custom_message = get_custom_message(
-            title=recipient[encoded_header_to_id[REGISTERED_TITLE]],
-            name=recipient[encoded_header_to_id[REGISTERED_NAME]],
-            receipt_num=recipient[encoded_header_to_id[RECEIPT_NUMBER]],
-            receipt_date=recipient[encoded_header_to_id[REGISTERED_DATE]],
-            gotra=recipient[encoded_header_to_id[REGISTERED_GOTRA]],
-            nakshatra=recipient[encoded_header_to_id[REGISTERED_NAKSHATRA]],
-            rashi=recipient[encoded_header_to_id[REGISTERED_RASHI]],
+            title=recipient[internalheader_to_columnid[SheetsHeader.REGISTERED_TITLE]],
+            name=recipient[internalheader_to_columnid[SheetsHeader.REGISTERED_NAME]],
+            receipt_num=recipient[
+                internalheader_to_columnid[SheetsHeader.RECEIPT_NUMBER]
+            ],
+            receipt_date=recipient[
+                internalheader_to_columnid[SheetsHeader.REGISTERED_DATE]
+            ],
+            gotra=recipient[internalheader_to_columnid[SheetsHeader.REGISTERED_GOTRA]],
+            nakshatra=recipient[
+                internalheader_to_columnid[SheetsHeader.REGISTERED_NAKSHATRA]
+            ],
+            rashi=recipient[internalheader_to_columnid[SheetsHeader.REGISTERED_RASHI]],
         )
         # TODO - Similarly send sms text
 
