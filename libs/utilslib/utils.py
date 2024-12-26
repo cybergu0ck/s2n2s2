ca02e649efbf2d249120062fe9af6993d583f144
agneshraok <agneshraok.apps@gmail.com>
2024-12-26 13:24:53 +0530

    Add function to generate html table

--- libs/utilslib/utils.py
+++ libs/utilslib/utils.py
@@ -25,3 +25,54 @@ def is_valid_date(value):
         return True
     except (ValueError, TypeError):
         return False
+
+
+def generate_html_table(two_d_array, headers):
+    """
+    Generates an HTML table from a 2D list with styled cells for better visibility.
+
+    Parameters:
+        two_d_array (list of list): A 2D list representing the table two_d_array.
+
+    Returns:
+        str: A string containing the HTML representation of the table.
+    """
+    if len(two_d_array) == 0:
+        return "Empty table"
+    html = """
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%; /* Full width */
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 20px; /* Increased padding for larger cells */
+            text-align: center; /* Center align text */
+            min-width: 100px; /* Minimum width for cells */
+        }
+    </style>
+    """
+    html += "<table>"
+
+    if len(two_d_array[0]) != len(headers):
+        html += "  <tr>\n"
+        for col_index in range(len(two_d_array[0])):
+            html += f"    <th>Column {col_index + 1}</th>\n"
+        html += "  </tr>\n"
+    else:
+        headers = headers
+        html += "  <tr>\n"
+        for header in headers:
+            html += f"    <th>{header}</th>\n"
+        html += "  </tr>\n"
+
+    for row in two_d_array:
+        html += "  <tr>\n"
+        for cell in row:
+            html += f"    <td>{cell}</td>\n"
+        html += "  </tr>\n"
+
+    html += "</table>"
+
+    return html
