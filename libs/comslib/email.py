5bd8a4f614f0ce57a99a39243b431582ade3f8ac
agneshraok <agneshraok.apps@gmail.com>
2024-12-26 22:38:38 +0530

    Update send_email function

--- libs/comslib/email.py
+++ libs/comslib/email.py
@@ -10,7 +10,7 @@ SENDER_EMAIL_ADDRESS = os.environ.get("S2N2S2-EMAIL")
 SENDER_EMAIL_KEY = os.environ.get("S2N2S2-EMAIL-KEY")
 
 
-def send_email(email_address, subject, body, attachements=[], is_html=False):
+def send_email(email_address, subject, body, attachements=[], is_html=False) -> bool:
     """
     Sends an email.
     Args:
@@ -19,27 +19,31 @@ def send_email(email_address, subject, body, attachements=[], is_html=False):
         body: Body of the email
         attachements : array of dict's containing path of the file to be attached and name to be given of the attached file.
     """
-    smtp_server = "smtp.gmail.com"
-    smtp_port = 465
-    context = ssl.create_default_context()
+    if is_valid_email(email_address):
+        smtp_server = "smtp.gmail.com"
+        smtp_port = 465
+        context = ssl.create_default_context()
 
-    message = MIMEMultipart("alternative")
-    message["Subject"] = subject
-    message["From"] = SENDER_EMAIL_ADDRESS
-    message["To"] = email_address
-    if is_html:
-        body_part = MIMEText(body, "html")
-        message.attach(body_part)
+        message = MIMEMultipart("alternative")
+        message["Subject"] = subject
+        message["From"] = SENDER_EMAIL_ADDRESS
+        message["To"] = email_address
+        if is_html:
+            body_part = MIMEText(body, "html")
+            message.attach(body_part)
+        else:
+            body_part = MIMEText(body, "plain")
+            message.attach(body_part)
+
+        for attachement in attachements:
+            with open(attachement["path"], "rb") as file:
+                message.attach(MIMEApplication(file.read(), Name=attachement["name"]))
+
+        with smtplib.SMTP_SSL(smtp_server, smtp_port, context=context) as server:
+            server.login(SENDER_EMAIL_ADDRESS, SENDER_EMAIL_KEY)
+            # server.sendmail(SENDER_EMAIL_ADDRESS, email_address, message.as_string())
+            server.send_message(message)
+            server.quit()
+        return True
     else:
-        body_part = MIMEText(body, "plain")
-        message.attach(body_part)
-
-    for attachement in attachements:
-        with open(attachement["path"], "rb") as file:
-            message.attach(MIMEApplication(file.read(), Name=attachement["name"]))
-
-    with smtplib.SMTP_SSL(smtp_server, smtp_port, context=context) as server:
-        server.login(SENDER_EMAIL_ADDRESS, SENDER_EMAIL_KEY)
-        # server.sendmail(SENDER_EMAIL_ADDRESS, email_address, message.as_string())
-        server.send_message(message)
-        server.quit()
+        return False
