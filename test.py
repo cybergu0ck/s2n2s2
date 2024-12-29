1be36cdc63e428a86bc4fd17bc2c81c74cc7aa89
agneshraok <agneshraok.apps@gmail.com>
2024-12-30 02:39:48 +0530

    Add test file

---
+++ test.py
@@ -0,0 +1,34 @@
+import serial
+import time
+
+PORT = "/dev/ttyS0"
+BAUD_RATE = 115200
+TIME_OUT = 1
+SERRIAL_OBJECT = serial.Serial(PORT, BAUD_RATE, timeout=TIME_OUT)
+
+# Send basic AT command, expect the response to be OK
+SERRIAL_OBJECT.write(f"AT\r")
+user_command = SERRIAL_OBJECT.readline().decode().strip()
+response = SERRIAL_OBJECT.readline().decode().strip()
+print(user_command)
+print(response)
+time.sleep(2)
+
+# Setting the mode or something
+# SERRIAL_OBJECT.write(f"AT+CMGF=1\r")
+# user_command = SERRIAL_OBJECT.readline().decode().strip()
+# response = SERRIAL_OBJECT.readline().decode().strip()
+# time.sleep(2)
+
+# # Sending Message
+# SERRIAL_OBJECT.write(f'AT+CMGS="{'9632448895'}"\r'.encode())
+# user_command = SERRIAL_OBJECT.readline().decode().strip()
+# response = SERRIAL_OBJECT.readline().decode().strip()
+# time.sleep(2)
+# SERRIAL_OBJECT.write(
+#             f"{"test message"}\x1A".encode()
+#         )  # \x1A is the ASCII code for Ctrl+Z
+# user_command = SERRIAL_OBJECT.readline().decode().strip()
+# response = SERRIAL_OBJECT.readline().decode().strip()
+# time.sleep(2)
+SERRIAL_OBJECT.close()
