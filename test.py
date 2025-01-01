1558b38019d86cf626c0ba2d292cc4a6f2f60d00
agneshraok <agneshraok.apps@gmail.com>
2025-01-02 01:08:48 +0530

    Save

--- test.py
+++ test.py
@@ -4,71 +4,121 @@ import time
 PORT = "/dev/ttyS0"
 BAUD_RATE = 115200
 TIME_OUT = 3
-SERRIAL_OBJECT = serial.Serial(PORT, BAUD_RATE, timeout=TIME_OUT)
+LTE_MODULE = serial.Serial(PORT, BAUD_RATE, timeout=TIME_OUT)
 
 
-def unicode_to_hex(text):
-    return "".join(f"{ord(char):04x}" for char in text).upper()
+def is_network_registered() -> bool:
+    """Returns True if the simcom lte module is registred to a network, else False."""
+    LTE_MODULE.write(b"AT+CREG?\r")
+    at_command = LTE_MODULE.readline().decode().strip()
+    print(f"AT Command to check network registration status: {at_command}")
+    response = LTE_MODULE.readline().decode().strip()
+    time.sleep(2)
+    # Successful output will be like  "+CREG: 0,1"
+    if response.startswith("+CREG:"):
+        status = response.split(",")[1]
+        if status == "0":
+            print(
+                f" Not registered,ME is not currently searching a new operator to register to."
+            )
+            print(f"Response : {response}")
+            return False
+        elif status == "1":
+            print(f"Registered to home network.")
+            print(f"Response : {response}")
+            return True
+        elif status == "2":
+            print(f"Registered to home network.")
+            print(f"Response : {response}")
+            return False
+        elif status == "3":
+            print(f"Registration denied.")
+            print(f"Response : {response}")
+            return False
+        elif status == "4":
+            print(f"Unknown response while fetching network registration.")
+            print(f"Response : {response}")
+            return False
+        elif status == "5":
+            print(f"Registered, roaming.")
+            print(f"Response : {response}")
+            return True
+        elif status == "6":
+            print(f"Registered, sms only.")
+            print(f"Response : {response}")
+            return True
+    else:
+        print(f"Unable to fetch network registation detail.")
+        print(f"Response : {response}")
+        return False
 
 
-SERRIAL_OBJECT.write(b"ATZ\r")
-user_command = SERRIAL_OBJECT.readline().decode().strip()
-response = SERRIAL_OBJECT.readline().decode().strip()
-print(user_command)
-print(response)
-time.sleep(2)
-
-SERRIAL_OBJECT.write(b'AT+CSCS="UCS2"\r')
-user_command = SERRIAL_OBJECT.readline().decode().strip()
-response = SERRIAL_OBJECT.readline().decode().strip()
-print(user_command)
-print(response)
-time.sleep(2)
-
-SERRIAL_OBJECT.write(b"AT+CSMP=17,168,0,8\r")
-user_command = SERRIAL_OBJECT.readline().decode().strip()
-response = SERRIAL_OBJECT.readline().decode().strip()
-print(user_command)
-print(response)
-time.sleep(2)
-
-SERRIAL_OBJECT.write(b"AT+CMGF=1\r")
-user_command = SERRIAL_OBJECT.readline().decode().strip()
-response = SERRIAL_OBJECT.readline().decode().strip()
-print(user_command)
-print(response)
-time.sleep(2)
+print(is_network_registered())
 
 
-phone_num = "+919632448895"
-# msg = "ತಮ್ಮ ಶಾಶ್ವತ ಸೇವಾ ಪೂಜೆಯನ್ನು ಇಂದು ನೆರವೇರಿಸಲಾಗಿದೆ."
-msg = "ನಿಮ್ಮ ನಾಲೂರು ಶಂಕರ ನಾರಾಯಣ ದೇವರ ಶಾಶ್ವತ ಪೂಜಾ ಸೇವೆ ಇಂದು ನಡೆಯಲಿದೆ"
+# def unicode_to_hex(text):
+#     return "".join(f"{ord(char):04x}" for char in text).upper()
 
-phone_num_hez = unicode_to_hex(phone_num)
-msg_hex = unicode_to_hex(msg)
+
+# LTE_MODULE.write(b"ATZ\r")
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
+# print(user_command)
+# print(response)
+# time.sleep(2)
+
+# LTE_MODULE.write(b'AT+CSCS="UCS2"\r')
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
+# print(user_command)
+# print(response)
+# time.sleep(2)
+
+# LTE_MODULE.write(b"AT+CSMP=17,168,0,8\r")
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
+# print(user_command)
+# print(response)
+# time.sleep(2)
+
+# LTE_MODULE.write(b"AT+CMGF=1\r")
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
+# print(user_command)
+# print(response)
+# time.sleep(2)
+
+
+# phone_num = "+919632448895"
+# # msg = "ತಮ್ಮ ಶಾಶ್ವತ ಸೇವಾ ಪೂಜೆಯನ್ನು ಇಂದು ನೆರವೇರಿಸಲಾಗಿದೆ."
+# msg = "ನಿಮ್ಮ ನಾಲೂರು ಶಂಕರ ನಾರಾಯಣ ದೇವರ ಶಾಶ್ವತ ಪೂಜಾ ಸೇವೆ ಇಂದು ನಡೆಯಲಿದೆ"
+
+# phone_num_hez = unicode_to_hex(phone_num)
+# msg_hex = unicode_to_hex(msg)
+
 
 # Sending Message
-print("Entering cmd to send message here")
-SERRIAL_OBJECT.write(f'AT+CMGS="{phone_num_hez}"\r'.encode())
-time.sleep(2)
-user_command = SERRIAL_OBJECT.readline().decode().strip()
-response = SERRIAL_OBJECT.readline().decode().strip()
-print(user_command)
-print(response)
-print("writing message here")
-SERRIAL_OBJECT.write(f"{msg_hex}\x1A".encode())  # \x1A is the ASCII code for Ctrl+Z
-user_command = SERRIAL_OBJECT.readline().decode().strip()
-response = SERRIAL_OBJECT.readline().decode().strip()
-print(user_command)
-print(response)
-print("1")
-response = SERRIAL_OBJECT.readline().decode().strip()
-print(response)
-print("2")
-response = SERRIAL_OBJECT.readline().decode().strip()
-print(response)
-time.sleep(2)
-SERRIAL_OBJECT.close()
+# print("Entering cmd to send message here")
+# LTE_MODULE.write(f'AT+CMGS="{phone_num_hez}"\r'.encode())
+# time.sleep(2)
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
+# print(user_command)
+# print(response)
+# print("writing message here")
+# LTE_MODULE.write(f"{msg_hex}\x1A".encode())  # \x1A is the ASCII code for Ctrl+Z
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
+# print(user_command)
+# print(response)
+# print("1")
+# response = LTE_MODULE.readline().decode().strip()
+# print(response)
+# print("2")
+# response = LTE_MODULE.readline().decode().strip()
+# print(response)
+# time.sleep(2)
+# LTE_MODULE.close()
 
 
 # # Prepare the message in Kannada
@@ -77,38 +127,38 @@ SERRIAL_OBJECT.close()
 #     "utf-16-be"
 # )  # Encode in UCS2 (UTF-16 Big Endian)
 
-# SERRIAL_OBJECT.write(f"AT+CMGS={len(encoded_message)//2}".encode() + b"\r")
+# LTE_MODULE.write(f"AT+CMGS={len(encoded_message)//2}".encode() + b"\r")
 # time.sleep(1)  # Wait for response
-# user_command = SERRIAL_OBJECT.readline().decode().strip()
-# response = SERRIAL_OBJECT.readline().decode().strip()
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
 # print(user_command)
 # print(response)
 
 # # Send the actual message
-# SERRIAL_OBJECT.write(f"{encoded_message}\x1A".encode())
+# LTE_MODULE.write(f"{encoded_message}\x1A".encode())
 # time.sleep(3)
-# user_command = SERRIAL_OBJECT.readline().decode().strip()
-# response = SERRIAL_OBJECT.readline().decode("utf-8").strip()
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode("utf-8").strip()
 # print("Final Response:", response)
 # print(user_command)
 # print(response)
 
-# SERRIAL_OBJECT.close()
+# LTE_MODULE.close()
 
 
 # # Sending Message
-# SERRIAL_OBJECT.write(f'AT+CMGS="{phone_num}"\r'.encode())
-# user_command = SERRIAL_OBJECT.readline().decode().strip()
-# response = SERRIAL_OBJECT.readline().decode().strip()
+# LTE_MODULE.write(f'AT+CMGS="{phone_num}"\r'.encode())
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
 # time.sleep(2)
-# SERRIAL_OBJECT.write(f"{msg}\x1A".encode("utf-8"))  # \x1A is the ASCII code for Ctrl+Z
-# user_command = SERRIAL_OBJECT.readline().decode().strip()
-# response = SERRIAL_OBJECT.readline().decode().strip()
+# LTE_MODULE.write(f"{msg}\x1A".encode("utf-8"))  # \x1A is the ASCII code for Ctrl+Z
+# user_command = LTE_MODULE.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
 # print(user_command)
 # print(response)
-# response = SERRIAL_OBJECT.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
 # print(response)
-# response = SERRIAL_OBJECT.readline().decode().strip()
+# response = LTE_MODULE.readline().decode().strip()
 # print(response)
 # time.sleep(2)
-# SERRIAL_OBJECT.close()
+# LTE_MODULE.close()
