89d7567db5bab2c419f323a06b981fc6b83d0d66
agneshraok <agneshraok.apps@gmail.com>
2025-01-03 19:43:11 +0530

    Save

--- test.py
+++ test.py
@@ -9,34 +9,35 @@ LTE_MODULE = serial.Serial(PORT, BAUD_RATE, timeout=TIME_OUT)
 
 def send_sms():
     phone_num = "+919632448895"
-    sms_message = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
-    LTE_MODULE.write(f'AT+CMGS="{phone_num}"\r'.encode())
-    at_command = LTE_MODULE.readline().decode().strip()
-    print(f"AT Command to send message : {at_command}")
-    response = LTE_MODULE.readline().decode().strip()
-    if response == ">":
-        LTE_MODULE.write(
-            f"{sms_message}\x1A".encode()
-        )  # \x1A is the ASCII code for Ctrl+Z
-        time.sleep(2)
+    sms_message = "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+    for i in range(1):
+        LTE_MODULE.write(f'AT+CMGS="{phone_num}"\r'.encode())
         at_command = LTE_MODULE.readline().decode().strip()
-        print(f"AT Command with message : {at_command}")
-        response_1 = LTE_MODULE.readline().decode().strip()
-        response_2 = LTE_MODULE.readline().decode().strip()
-        response_3 = LTE_MODULE.readline().decode().strip()
-        if response_1.startswith("+CMGS:") and response_3 == "OK":
-            print(f"Response : {response_1}")
-            print(f"Response : {response_2}")
-            print(f"Response : {response_3}")
-            return True
+        print(f"AT Command to send message : {at_command}")
+        response = LTE_MODULE.readline().decode().strip()
+        if response == ">":
+            LTE_MODULE.write(
+                f"{sms_message}\x1A".encode()
+            )  # \x1A is the ASCII code for Ctrl+Z
+            time.sleep(2)
+            at_command = LTE_MODULE.readline().decode().strip()
+            print(f"AT Command with message : {at_command}")
+            response_1 = LTE_MODULE.readline().decode().strip()
+            response_2 = LTE_MODULE.readline().decode().strip()
+            response_3 = LTE_MODULE.readline().decode().strip()
+            if response_1.startswith("+CMGS:") and response_3 == "OK":
+                print(f"Response : {response_1}")
+                print(f"Response : {response_2}")
+                print(f"Response : {response_3}")
+                return True
+            else:
+                print(f"Message not sent.")
+                print(f"Response : {response_1}")
+                return False
         else:
             print(f"Message not sent.")
-            print(f"Response : {response_1}")
+            print(f"Response : {response}")
             return False
-    else:
-        print(f"Message not sent.")
-        print(f"Response : {response}")
-        return False
 
 
 send_sms()
