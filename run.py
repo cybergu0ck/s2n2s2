a55221d0251ca9592162ceeb6c1ff7c5ae8cf747
cybergu0ck <agneshraok.apps@gmail.com>
2025-03-30 13:06:50 +0530

    test commit

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from libs.comslib.sms import *
 phone_nums = ["+919632448895"]
 
 for i in range(10):
-    dispatch_sms(phone_nums[0], "Hello, This is test message")
+    dispatch_sms(phone_nums[0], f"Test message {i}")
