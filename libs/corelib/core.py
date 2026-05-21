c3243f9d7f81889fafc821964bd58e9a608b3700
cybergu0ck <agneshraok.apps@gmail.com>
2026-05-21 11:56:36 +0530

    fix: update english message as given by Yogesh Chikka

--- libs/corelib/core.py
+++ libs/corelib/core.py
@@ -325,7 +325,7 @@ def get_simple_kannada_message():
 
 
 def get_simple_english_message():
-    return "On this day of your Shashwatha Pooja Seva, may the blessings of Shri Shankara Narayana Swamy be with you always - Naloor, Kadaba"
+    return "On this auspicious day of your Shashwatha Pooja Seva, may Shri Shankara Narayana Swamy bless you and your family with peace, prosperity and happiness - Naloor, Kadaba"
 
 
 def get_message_for_purohit(recipients):
