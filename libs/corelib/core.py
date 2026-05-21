90659eb2c4ebbf431459c4ea8929ae6b5763ef50
cybergu0ck <agneshraok.apps@gmail.com>
2026-05-21 12:00:52 +0530

    fix: shorten the english message as per sms module capacity

--- libs/corelib/core.py
+++ libs/corelib/core.py
@@ -325,7 +325,7 @@ def get_simple_kannada_message():
 
 
 def get_simple_english_message():
-    return "On this auspicious day of your Shashwatha Pooja Seva, may Shri Shankara Narayana Swamy bless you and your family with peace, prosperity and happiness - Naloor, Kadaba"
+    return "On this auspicious day of your Shashwatha Pooja Seva, may Shri Shankara Narayana Swamy bless you and your family with peace and happiness - Naloor, Kadaba"
 
 
 def get_message_for_purohit(recipients):
